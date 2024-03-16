de6bbb17af8a0bf9c3ab93f5489a8dd869715e40
PberruezoArenas <149729949+PberruezoArenas@users.noreply.github.com>
2024-03-16 11:15:34 +0000

    added last relationships

--- src/models.py
+++ src/models.py
@@ -7,23 +7,50 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+
+class Favorites(Base):
+    __tablename__ = 'favorites'
+    id = Column(Integer, primary_key=True)
+    people_id = Column(Integer, ForeignKey('people.id'), nullable=True)
+    planets_id = Column(Integer, ForeignKey('planets.id'), nullable=True)
+    starships_id = Column(Integer, ForeignKey('starships.id'), nullable=True)
+    users_id = Column(Integer, ForeignKey('users.id'),nullable=False)
+
+
+class Users(Base):
+    __tablename__='users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    email = Column(String(250))
+
+
+class People(Base):
+    __tablename__ = 'people'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    eye_color = Column(String(250), nullable=False)
+    height = Column(Integer, nullable=False)
+    PeopleFavorites = relationship(Favorites)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
+class Planets(Base):
+    __tablename__ = 'planets'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    climate = Column(String(250), nullable=False)
+    population = Column(Integer, nullable=False)
+    PlanetsFavorites = relationship(Favorites)
+
+
+class Starships(Base):
+    __tablename__ = 'starships'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    StarshipsFavorites = relationship(Favorites)
+
+
 
     def to_dict(self):
         return {}
